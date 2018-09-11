cef0aba51f09b387ae86fb1ffe1124ea8ab06b26
kantonist <39885423+kantonist@users.noreply.github.com>
2018-09-11 15:34:27 -0400

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,12 @@
+first = 5
+second = 81
+# first=x and second = y
+first = first - second
+# first = x - y
+second = second + first
+# second = y + x - y = x
+first = second - first
+# first = x - x + y = y
+print('first = ', first)
+print('secont = ', second)
+
